14cce91b79781cfac59a2a9b8056bad4a3b9fa37
Pataxsa <pat.cvjetic17@gmail.com>
2022-01-07 23:14:24 +0100

    MAJ

--- classes.py
+++ classes.py
@@ -5,38 +5,74 @@ class Game:
 
     def __init__(self, screen):
         self.isplaying = False
-        self.pendu = Pendu(screen)
-        self.clavier = Clavier(screen)
-        self.bar = pygame.transform.scale(pygame.image.load('assets/images/bar.png'), (40, 40))
+        self.pendu = Hanged(screen)
+        self.clavier = KeyBoard(screen)
         self.foundwords = []
-        self.possiblewords = ["axel"]
+        self.possiblewords = ["bed", "computer", "0BL1V10N", "incredible", "destroy", "cat", "potato", "developper", "minecraft"]
+        self.endgame = {"winned": False, "ended": False}
     
     def update(self, screen, word):
-        words = 0
+        wordsspace = 0
+        validwords = 0
 
         screen.blit(self.pendu.image, self.pendu.rect)
         screen.blit(self.clavier.image, self.clavier.rect)
+
+        self.pendu.image = pygame.image.load(f'assets/images/hanged_man_parts/hanged_man_part_{self.pendu.parts}.png')
         for wrd in word:
-            words += 40
+            wordsspace += 40
+            wrd = wrd.lower()
 
-            screen.blit(self.bar, (280 + words,  math.ceil((screen.get_height()-self.bar.get_height())/2)))
+            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(235 + wordsspace,  math.ceil((screen.get_height()/2)), 30, 6))
             if wrd in self.foundwords:
+                validwords += 1
                 font = pygame.font.SysFont('Sans', 40)
-                text = font.render(wrd, True, (255, 0, 0))
-                
-                screen.blit(text, (290 + words,  math.ceil((screen.get_height()-self.bar.get_height())/2) - 29))
+                text = font.render(wrd.upper(), True, (60, 60, 60))
+                    
+                screen.blit(text, (240 + wordsspace,  math.ceil((screen.get_height()/2)) - 43))
 
-class Pendu(pygame.sprite.Sprite):
+        if validwords == len(word):
+            self.endgame["ended"] = True
+            self.endgame["winned"] = True
+        elif self.pendu.parts == 6:
+                self.endgame["ended"] = True
+                self.endgame["winned"] = False
+            
+
+    def end(self, screen, word, button, button_rect):
+        if self.endgame["winned"]:
+            font = pygame.font.SysFont('Sans', 40)
+            text = font.render("You win !", True, (60, 60, 60))
+            text_rect = text.get_rect()
+            text_rect.x = math.ceil((screen.get_width()-text.get_width())/2)
+            text_rect.y = math.ceil((screen.get_height()-text.get_height())/3.5)
+        else:
+            font = pygame.font.SysFont('Sans', 40)
+            text = font.render("You lost !", True, (60, 60, 60))
+            text_rect = text.get_rect()
+            text_rect.x = math.ceil((screen.get_width()-text.get_width())/2)
+            text_rect.y = math.ceil((screen.get_height()-text.get_height())/3.5)
+
+        text2 = font.render(f"The word was: {word}", True, (60, 60, 60))
+        text2_rect = text2.get_rect()
+        text2_rect.x = math.ceil((screen.get_width()-text2.get_width())/2)
+        text2_rect.y = math.ceil((screen.get_height()-text2.get_height())/2.5)
+                    
+        screen.blit(text, text_rect)
+        screen.blit(text2, text2_rect)
+        screen.blit(button, button_rect)
+
+class Hanged():
 
     def __init__(self, screen):
         super().__init__()
         self.parts = 0
-        self.image = pygame.image.load('assets/images/hanged_man.jpg')
+        self.image = pygame.image.load('assets/images/hanged_man_parts/hanged_man_part_0.png')
         self.rect = self.image.get_rect()
         self.rect.x = math.ceil((screen.get_width()-self.image.get_width())/5)
         self.rect.y = math.ceil((screen.get_height()-self.image.get_height())/2)-40
 
-class Clavier():
+class KeyBoard():
 
     def __init__(self, screen):
         super().__init__()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import randint
 
 pygame.init()
 
-pygame.display.set_caption("Le pendu")
+pygame.display.set_caption("Hanged Man Game")
 pygame.display.set_icon(pygame.image.load('assets/images/logo.jpg'))
 screen = pygame.display.set_mode((700, 431))
 background = pygame.image.load('assets/images/background.jpg')
@@ -14,13 +14,38 @@ button = pygame.transform.scale(button, (200, 200))
 button_rect = button.get_rect()
 button_rect.x = math.ceil((screen.get_width()-button.get_width())/2)
 button_rect.y = math.ceil((screen.get_height()-button.get_height())/2)
-buttons = {
-    "a": pygame.rect.Rect(125, 314, 30, 30),
-    "b": pygame.rect.Rect(160, 314, 30, 30),
-    "c": pygame.rect.Rect(195, 314, 30, 30),
-    "d": pygame.rect.Rect(195+35, 314, 30, 30),
-    "e": pygame.rect.Rect(195+35+35, 314, 30, 30)
-}
+
+button2 = pygame.image.load('assets/images/mainmenubutton.png')
+button2 = pygame.transform.scale(button2, (200, 57))
+button_rect2 = button2.get_rect()
+button_rect2.x = math.ceil((screen.get_width()-button2.get_width())/2)
+button_rect2.y = math.ceil((screen.get_height()-button2.get_height())/1.8)
+
+allwords = "abcdefghijklmnopqrstuvwxyz0123456789"
+buttons = {}
+
+block = [125, 315]
+start_pos = [125, 315]
+end_pos = [153, 343]
+repeat = 0
+for wrd in allwords:
+    repeat += 1
+    if repeat == 14:
+        block = [125, 350]
+        start_pos = [125, 350]
+        end_pos = [153, 378]
+    if repeat == 27:
+        block = [125, 385]
+        start_pos = [125, 385]
+        end_pos = [153, 413]
+    buttons[wrd] = {}
+    buttons[wrd]["block"] = pygame.Rect(block[0], block[1], 30, 30)
+    buttons[wrd]["start_pos"] = (start_pos[0], start_pos[1])
+    buttons[wrd]["end_pos"] = (end_pos[0], end_pos[1])
+    buttons[wrd]["closed"] = False
+    block[0] += 35.75
+    start_pos[0] += 35.75
+    end_pos[0] += 35.75
 
 running = True
 
@@ -33,7 +58,16 @@ while running:
     screen.blit(background, (0, 0))
 
     if game.isplaying:
-        game.update(screen, word)
+        if game.endgame["ended"] == True:
+            game.end(screen, word, button2, button_rect2)
+        else:
+            game.update(screen, word)
+            for key in buttons:
+                    if buttons[key]["closed"] == True:
+                        pygame.draw.line(screen, (255, 0, 0), buttons[key]["start_pos"], buttons[key]["end_pos"])
+                        start_pos = (buttons[key]["start_pos"][0] + 28, buttons[key]["start_pos"][1])
+                        end_pos = (buttons[key]["end_pos"][0] - 28, buttons[key]["end_pos"][1])
+                        pygame.draw.line(screen, (255, 0, 0), start_pos, end_pos)
     else:
         screen.blit(button, button_rect)
     
@@ -43,14 +77,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-            print("Jeu fermé")
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
-            if button_rect.collidepoint(event.pos) and not game.isplaying:
-                print("Play !")
+            if button_rect.collidepoint(event.pos) and not game.isplaying and not game.endgame["ended"]:
                 game.isplaying = True
+            if button_rect2.collidepoint(event.pos) and game.isplaying and game.endgame["ended"]:
+                game.isplaying = False
+                game.endgame["ended"] = False
+                game.endgame["winned"] = False
+                game.foundwords = []
+                game.pendu.parts = 0
+                game.pendu.image = pygame.image.load(f'assets/images/hanged_man_parts/hanged_man_part_{game.pendu.parts}.png')
+                for key in buttons:
+                    if buttons[key]["closed"] == True:
+                        buttons[key]["closed"] = False
+                rand = randint(0, len(game.possiblewords)-1)
+                word = game.possiblewords[rand]
             for key in buttons:
-                if buttons[key].collidepoint(event.pos) and game.isplaying:
-                    print(key)
+                if buttons[key]["block"].collidepoint(event.pos) and game.isplaying and not game.endgame["ended"]:
                     if not key in game.foundwords:
+                        buttons[key]["closed"] = True
                         game.foundwords.append(key)
+                        if not key in word:
+                            game.pendu.parts += 1
